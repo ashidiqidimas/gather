dac6d1379569876de8c461c02c586a54b82da0d7
ashidiqidimas <prabowo6@gmail.com>
2022-04-11 12:55:18 +0700

    Finished buildCardHeader function and added pin icons to assets

--- lib/ui/styles/gather_text_style.dart
+++ lib/ui/styles/gather_text_style.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
 class GatherTextStyle {
+  /// Use for postName in PostCard
   static TextStyle subhead2(BuildContext context) {
     return Theme.of(context).textTheme.labelLarge!.copyWith(
           fontSize: 17,
-          height: 1.25,
+          height: 1.29,
           fontWeight: FontWeight.w600,
           letterSpacing: 0.5,
         );
   }
 
+  /// Use for location in PostName
   static TextStyle caption(BuildContext context) {
     return Theme.of(context).textTheme.labelSmall!.copyWith(
-          fontSize: 15,
-          height: 1.23,
+          fontSize: 13,
+          height: 1.15,
           fontWeight: FontWeight.w500,
           letterSpacing: 0,
         );
@@ -21,8 +23,8 @@ class GatherTextStyle {
 
   static TextStyle callout(BuildContext context) {
     return Theme.of(context).textTheme.labelMedium!.copyWith(
-          fontSize: 15,
-          height: 1.23,
+          fontSize: 14,
+          height: 1.35,
           fontWeight: FontWeight.w500,
           letterSpacing: 0,
         );
--- lib/ui/widgets/post_card.dart
+++ lib/ui/widgets/post_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gather/ui/styles/gather_text_style.dart';
 
 class PostCard extends StatelessWidget {
   const PostCard({Key? key}) : super(key: key);
@@ -6,7 +7,7 @@ class PostCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
+      // color: Colors.yellow,
       padding: const EdgeInsets.all(16),
       child: Column(
         children: [
@@ -21,10 +22,38 @@ Widget buildCardHeader(BuildContext context) {
   return Row(
     mainAxisAlignment: MainAxisAlignment.start,
     crossAxisAlignment: CrossAxisAlignment.center,
-    children: const [
-      CircleAvatar(
+    children: [
+      const CircleAvatar(
         backgroundImage: AssetImage('assets/dummy/post/profilePicture.png'),
         radius: 20,
+      ),
+      const SizedBox(
+        width: 6,
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'dhamar',
+            style: GatherTextStyle.subhead2(context),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset(
+                'assets/shared/icons/pin.png',
+                height: 14,
+              ),
+              const SizedBox(
+                width: 4,
+              ),
+              Text(
+                'Sentul, Jawa Barat',
+                style: GatherTextStyle.caption(context),
+              ),
+            ],
+          ),
+        ],
       ),
     ],
   );
